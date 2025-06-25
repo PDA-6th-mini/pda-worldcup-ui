50872ddf23f7389a6218cc6e58fc95ca8cf45adf
임채연 <dlacodus0407@gmail.com>
2025-06-25 16:10:09 +0900

    feat : 백엔드 api에 연결한다
    - mock 데이터를 삭제한다

--- src/containers/mainPage/MainPage.tsx
+++ src/containers/mainPage/MainPage.tsx
@@ -12,65 +12,31 @@ type Card = {
 	thumbNail_2: string;
 };
 
-const mockProblems = [
-	{
-		problem_id: 1,
-		name: '문제 A',
-		description: null,
-		images: [
-			{
-				img_id: 3,
-				img_url: 'a3.jpg',
-			},
-			{
-				img_id: 1,
-				img_url: 'a1.jpg',
-			},
-			{
-				img_id: 2,
-				img_url: 'a2.jpg',
-			},
-		],
-	},
-	{
-		problem_id: 2,
-		name: '문제 B',
-		description: '이런거 나 조하애',
-		images: [
-			{
-				img_id: 4,
-				img_url: 'b1.jpg',
-			},
-			{
-				img_id: 5,
-				img_url: 'b2.jpg',
-			},
-			{
-				img_id: 6,
-				img_url: 'b3.jpg',
-			},
-		],
-	},
-];
-
 export const MainPageContainer = () => {
-	const [problems, setProblems] = useState<Card[]>([]);
+	const [problems, setProblems] = useState<any[]>([]);
 
 	useEffect(() => {
-		const parsed: Card[] = mockProblems.map((problem) => {
-			const images = problem.images;
+		const getProblemData = async () => {
+			const response = await fetch('http://localhost:3000/api/main');
+			const responseBody = await response.json();
 
-			const data: Card = {
-				id: problem.problem_id,
-				title: problem.name,
-				description: problem.description,
-				thumbNail_1: images[0].img_url,
-				thumbNail_2: images[1].img_url,
-			};
+			const parsed: Card[] = responseBody.data.map((problem: any) => {
+				const images = problem.images;
 
-			return data;
-		});
-		setProblems(parsed);
+				const data: Card = {
+					id: problem.problem_id,
+					title: problem.name,
+					description: problem.description,
+					thumbNail_1: images[0].img_url,
+					thumbNail_2: images[1].img_url,
+				};
+				return data;
+			});
+
+			setProblems(parsed);
+		};
+
+		getProblemData();
 	}, []);
 
 	return (
