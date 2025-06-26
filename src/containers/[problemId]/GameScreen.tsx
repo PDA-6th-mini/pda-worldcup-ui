7eb647a1e872c1b8fcce75858270b97e77abf9b7
Jihwan Lee <wlghks0508@naver.com>
2025-06-26 13:00:40 +0900

    #17 db connect, fetch

--- src/containers/[problemId]/GameScreen.tsx
+++ src/containers/[problemId]/GameScreen.tsx
@@ -1,5 +1,6 @@
 import MatchView from '@/components/GameScreen';
 import { Container, Row, Col } from 'react-bootstrap';
+import { fetchData } from '@/services/user-service';
 
 export interface GameScreenContainerProps {
 	params: Promise<{
@@ -10,20 +11,21 @@ export default async function GameScreenContainer({
 	params,
 }: GameScreenContainerProps) {
 	const { problemId } = await params;
-	console.log('problem_id => ', problemId);
+	const { data } = await fetchData(problemId);
+
 	return (
 		<div className="bg-black w-100 min-vh-100 d-flex align-items-center justify-content-center">
 			<Container className="bg-black text-white d-flex flex-column align-items-center justify-content-start pt-4 m-0 p-0">
 				{/* 상단 타이틀 */}
 				<Row className="text-center mb-4">
 					<Col>
-						<h1 className="fs-3 fw-bold">{problemId}</h1>
+						<h1 className="fs-3 fw-bold">{data[0].name}</h1>
 						<br />
 					</Col>
 				</Row>
 				<Row>
 					<Col>
-						<MatchView />
+						<MatchView fetchData={data} />
 					</Col>
 				</Row>
 			</Container>
