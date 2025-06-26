24e6ba8816d4270a5159ce86b71a2c1d9affb5cd
임채연 <dlacodus0407@gmail.com>
2025-06-26 11:29:02 +0900

    feat : 메인페이지 ui 작업

--- src/components/MainPageCard/ListCard.tsx
+++ src/components/MainPageCard/ListCard.tsx
@@ -21,23 +21,80 @@ export const ListCard = ({
 	const router = useRouter();
 
 	return (
-		<Card className="h-100 shadow-sm">
-			<Card.Img variant="top" src={thumbNail_1} alt={`${title} 이미지`} />
-			<Card.Img variant="top" src={thumbNail_2} alt={`${title} 이미지`} />
-			<Card.Body>
-				<Card.Title>{title}</Card.Title>
-				<Card.Text>{description}</Card.Text>
-				<ButtonGroup>
+		<Card
+			style={{
+				width: '100%',
+				maxWidth: '700px',
+				margin: '0 auto',
+				height: 'auto',
+				marginBottom: '0.5rem',
+				borderRadius: '0px',
+			}}
+			className="shadow-sm"
+		>
+			<div style={{ display: 'flex', width: '100%' }}>
+				<Card.Img
+					variant="top"
+					src={thumbNail_1}
+					alt={`${title} 이미지`}
+					style={{
+						width: '50%',
+						height: 280,
+						objectFit: 'cover',
+						borderRadius: 0,
+					}}
+				/>
+				<Card.Img
+					variant="top"
+					src={thumbNail_2}
+					alt={`${title} 이미지`}
+					style={{
+						width: '50%',
+						height: 280,
+						objectFit: 'cover',
+						borderRadius: 0,
+					}}
+				/>
+			</div>
+			<Card.Body
+				style={{
+					padding: '0.75rem',
+				}}
+			>
+				<Card.Title style={{ fontSize: '1rem' }}>{title}</Card.Title>
+				<Card.Text style={{ fontSize: '0.85rem' }}>{description}</Card.Text>
+				<ButtonGroup size="sm">
 					<Button
 						variant="primary"
-						onClick={() => router.push(`/problem/${id}`)}
+						onClick={() => router.push(`/${id}`)}
+						style={{
+							background: 'white',
+							color: '#03045E',
+							borderColor: '#03045E',
+						}}
 					>
 						시작하기
 					</Button>
-					<Button variant="primary" onClick={() => router.push('')}>
+					<Button
+						variant="primary"
+						onClick={() => router.push('')}
+						style={{
+							backgroundColor: 'white',
+							color: '#023E8A',
+							borderColor: '#023E8A',
+						}}
+					>
 						랭킹보기
 					</Button>
-					<Button variant="primary" onClick={() => router.push('')}>
+					<Button
+						variant="primary"
+						onClick={() => router.push('')}
+						style={{
+							color: '#0077B6',
+							background: 'white',
+							borderColor: '#0077B6',
+						}}
+					>
 						공유하기
 					</Button>
 				</ButtonGroup>
